34de2bae768797c28d551aff7d713889b163d9d6
Drosztmér István <drosztmer.istvan@gmail.com>
2021-02-23 12:45:14 +0100

    Setup navcontroller, navigation, bottomnavigationview

--- app/src/main/java/com/codecool/morick/ui/fragments/CharactersFragment.kt
+++ app/src/main/java/com/codecool/morick/ui/fragments/CharactersFragment.kt
@@ -1,20 +1,52 @@
 package com.codecool.morick.ui.fragments
 
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.codecool.morick.R
+import com.codecool.morick.databinding.FragmentCharactersBinding
 
-class CharactersFragment : Fragment() {
+class CharactersFragment : Fragment(), SearchView.OnQueryTextListener {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ): View {
+        _binding = FragmentCharactersBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.characters_menu, menu)
+
+        val search = menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as? SearchView
+        searchView?.isSubmitButtonEnabled = true
+        searchView?.setOnQueryTextListener(this)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if (query != null) {
+
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
