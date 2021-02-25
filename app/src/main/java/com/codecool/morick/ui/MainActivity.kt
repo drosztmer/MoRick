acf01012c85b599fc8e8720619fdbc44b4f1c2eb
Drosztmér István <drosztmer.istvan@gmail.com>
2021-02-25 12:07:59 +0100

    Add AndroidEntryPoint annotation

--- app/src/main/java/com/codecool/morick/ui/MainActivity.kt
+++ app/src/main/java/com/codecool/morick/ui/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.codecool.morick.R
 import com.codecool.morick.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
