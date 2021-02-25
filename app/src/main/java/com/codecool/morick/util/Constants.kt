40edf804fbc76206076b506702945369b95cf1f1
Drosztmér István <drosztmer.istvan@gmail.com>
2021-02-25 11:57:36 +0100

    Show network status / data

--- app/src/main/java/com/codecool/morick/util/Constants.kt
+++ app/src/main/java/com/codecool/morick/util/Constants.kt
@@ -6,5 +6,8 @@ class Constants {
 
         const val BASE_URL = "https://rickandmortyapi.com/api/"
 
+        const val PREFERENCES_NAME = "rick_and_morty_preferences"
+        const val PREFERENCES_BACK_ONLINE = "backOnline"
+
     }
 }
