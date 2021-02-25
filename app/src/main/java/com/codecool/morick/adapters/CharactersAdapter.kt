2278dc9339024f12cc75a2fc3fa20ea00e10098d
Drosztmér István <drosztmer.istvan@gmail.com>
2021-02-25 10:52:56 +0100

    Add setData function

--- app/src/main/java/com/codecool/morick/adapters/CharactersAdapter.kt
+++ app/src/main/java/com/codecool/morick/adapters/CharactersAdapter.kt
@@ -2,9 +2,12 @@ package com.codecool.morick.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.codecool.morick.databinding.ItemCharacterBinding
 import com.codecool.morick.models.RickAndMortyCharacter
+import com.codecool.morick.models.RickAndMortyResponse
+import com.codecool.morick.util.CharactersDiffUtil
 
 class CharactersAdapter: RecyclerView.Adapter<CharactersAdapter.MyViewHolder>() {
 
@@ -32,5 +35,12 @@ class CharactersAdapter: RecyclerView.Adapter<CharactersAdapter.MyViewHolder>() 
 
     override fun getItemCount(): Int {
         return characters.size
+    }
+
+    fun setData(newResponse: RickAndMortyResponse) {
+        val charactersDiffUtil = CharactersDiffUtil(characters, newResponse.results)
+        val diffUtilResult = DiffUtil.calculateDiff(charactersDiffUtil)
+        characters = newResponse.results
+        diffUtilResult.dispatchUpdatesTo(this)
     }
 }
