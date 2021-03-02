a5ebadcd5ca97ee09d9c495d1dafc0853ebb588b
Drosztmér István <drosztmer.istvan@gmail.com>
2021-03-02 11:54:11 +0100

    Implement navigating to details page

--- app/src/main/java/com/codecool/morick/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/codecool/morick/ui/fragments/DetailsFragment.kt
@@ -5,16 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.codecool.morick.R
+import androidx.navigation.fragment.navArgs
+import coil.load
+import com.codecool.morick.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    private val args by navArgs<DetailsFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        val character = args.character
+        binding.detailImage.load(character.image) {
+            crossfade(600)
+        }
+        return binding.root
     }
 
 }
