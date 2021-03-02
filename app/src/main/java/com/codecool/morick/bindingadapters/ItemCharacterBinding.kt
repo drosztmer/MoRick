e26622f3021d41c04e07baf6a99d3fe346ce5ba4
Drosztmér István <drosztmer.istvan@gmail.com>
2021-03-02 11:14:56 +0100

    Add bindingadapter function

--- app/src/main/java/com/codecool/morick/bindingadapters/ItemCharacterBinding.kt
+++ app/src/main/java/com/codecool/morick/bindingadapters/ItemCharacterBinding.kt
@@ -1,9 +1,15 @@
 package com.codecool.morick.bindingadapters
 
+import android.util.Log
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.codecool.morick.R
+import com.codecool.morick.models.RickAndMortyCharacter
+import com.codecool.morick.ui.fragments.CharactersFragmentDirections
+import java.lang.Exception
 
 class ItemCharacterBinding {
 
@@ -15,6 +21,19 @@ class ItemCharacterBinding {
             imageView.load(imageUrl) {
                 crossfade(600)
                 error(R.drawable.ic_error_placeholder)
+            }
+        }
+
+        @BindingAdapter("onCharacterClickListener")
+        @JvmStatic
+        fun onCharacterClickListener(itemCharacterLayout: ConstraintLayout, character: RickAndMortyCharacter) {
+            itemCharacterLayout.setOnClickListener {
+                try {
+                    val action = CharactersFragmentDirections.actionCharactersFragmentToDetailsFragment(character)
+                    itemCharacterLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("onCharacterClickListener", e.toString())
+                }
             }
         }
 
