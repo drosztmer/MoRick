723e2a2f7099d6479a5f3b6556cc284143f1ee21
Drosztmér István <drosztmer.istvan@gmail.com>
2021-03-04 12:11:06 +0100

    Add menu to fragment

--- app/src/main/java/com/codecool/morick/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/codecool/morick/ui/fragments/DetailsFragment.kt
@@ -1,12 +1,12 @@
 package com.codecool.morick.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
+import com.codecool.morick.R
 import com.codecool.morick.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
@@ -16,6 +16,10 @@ class DetailsFragment : Fragment() {
 
     private val args by navArgs<DetailsFragmentArgs>()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +31,26 @@ class DetailsFragment : Fragment() {
             crossfade(600)
         }
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.details_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.details_favorite -> {
+                Toast.makeText(requireContext(), "Favorite!", Toast.LENGTH_SHORT).show()
+            }
+            R.id.details_share -> {
+                Toast.makeText(requireContext(), "Share!", Toast.LENGTH_SHORT).show()
+            }
+            R.id.details_save -> {
+                Toast.makeText(requireContext(), "Save!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
