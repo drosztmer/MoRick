8f8acd53533b28472469c7c4c638e1b648e7f169
Drosztmér István <drosztmer.istvan@gmail.com>
2021-03-05 11:43:14 +0100

    Improve layout

--- app/src/main/java/com/codecool/morick/bindingadapters/DetailsBinding.kt
+++ app/src/main/java/com/codecool/morick/bindingadapters/DetailsBinding.kt
@@ -1,10 +1,22 @@
 package com.codecool.morick.bindingadapters
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import coil.load
+import com.codecool.morick.R
+
 class DetailsBinding {
 
     companion object {
 
-
+        @BindingAdapter("loadDetailsImageFromUrl")
+        @JvmStatic
+        fun loadDetailsImageFromUrl(imageView: ImageView, imageUrl: String) {
+            imageView.load(imageUrl) {
+                crossfade(600)
+                error(R.drawable.ic_error_placeholder)
+            }
+        }
 
     }
 }
