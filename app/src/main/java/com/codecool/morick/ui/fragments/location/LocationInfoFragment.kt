8412c201fbd1776b3e716d26d83560b14056af6e
Drosztmér István <drosztmer.istvan@gmail.com>
2021-03-08 11:23:13 +0100

    Use data binding in LocationInfoFragment

--- app/src/main/java/com/codecool/morick/ui/fragments/location/LocationInfoFragment.kt
+++ app/src/main/java/com/codecool/morick/ui/fragments/location/LocationInfoFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.codecool.morick.R
+import com.codecool.morick.databinding.FragmentLocationInfoBinding
+import com.codecool.morick.models.RickAndMortyLocation
+import com.codecool.morick.util.Constants.Companion.LOCATION_BUNDLE
 
 class LocationInfoFragment : Fragment() {
+
+    private var _binding: FragmentLocationInfoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location_info, container, false)
+        _binding = FragmentLocationInfoBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val location: RickAndMortyLocation? = args?.getParcelable(LOCATION_BUNDLE)
+
+        binding.location = location
+
+        return binding.root
     }
 
 }
