2a516d1af951f317e6f9af584095f186fbb5c140
Drosztmér István <drosztmer.istvan@gmail.com>
2021-03-09 11:06:54 +0100

    Get arguments

--- app/src/main/java/com/codecool/morick/ui/fragments/location/LocationCharactersFragment.kt
+++ app/src/main/java/com/codecool/morick/ui/fragments/location/LocationCharactersFragment.kt
@@ -1,20 +1,39 @@
 package com.codecool.morick.ui.fragments.location
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.codecool.morick.R
+import com.codecool.morick.databinding.FragmentLocationCharactersBinding
+import com.codecool.morick.models.RickAndMortyLocation
+import com.codecool.morick.util.Constants
+import com.codecool.morick.util.Util
 
 class LocationCharactersFragment : Fragment() {
+
+    private var _binding: FragmentLocationCharactersBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location_characters, container, false)
+        _binding = FragmentLocationCharactersBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val location: RickAndMortyLocation? = args?.getParcelable(Constants.LOCATION_BUNDLE)
+        val locationIdQuery = location?.let { Util.getIdQueryFromUrls(it.residents) }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
