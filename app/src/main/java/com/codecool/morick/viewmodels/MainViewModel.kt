3fb4154323fe88dc9b17d134e6db45a4438cdc7a
Drosztmér István <drosztmer.istvan@gmail.com>
2021-03-09 11:18:54 +0100

    Add functions to MainViewModel to get multiple characters

--- app/src/main/java/com/codecool/morick/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/codecool/morick/viewmodels/MainViewModel.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.viewModelScope
 import com.codecool.morick.data.DataStoreRepository
 import com.codecool.morick.data.Repository
 import com.codecool.morick.models.Location
+import com.codecool.morick.models.RickAndMortyCharacter
 import com.codecool.morick.models.RickAndMortyLocation
 import com.codecool.morick.models.RickAndMortyResponse
 import com.codecool.morick.util.NetworkResult
@@ -38,6 +39,8 @@ class MainViewModel @Inject constructor(
 
     val isLocationLoaded: MutableLiveData<Boolean> = MutableLiveData(false)
 
+    val multipleCharacters: MutableLiveData<NetworkResult<List<RickAndMortyCharacter>>> = MutableLiveData()
+
     val readBackOnline = dataStoreRepository.readBackOnline.asLiveData()
 
     var networkStatus = false
@@ -53,6 +56,10 @@ class MainViewModel @Inject constructor(
 
     fun getLocationById(locationId: String) = viewModelScope.launch {
         getLocationByIdSafeCall(locationId)
+    }
+
+    fun getMultipleCharacters(idList: String) = viewModelScope.launch {
+        getMultipleCharactersSafeCall(idList)
     }
 
     fun saveBackOnline(backOnline: Boolean) = viewModelScope.launch(Dispatchers.IO) {
@@ -102,6 +109,20 @@ class MainViewModel @Inject constructor(
         }
     }
 
+    private suspend fun getMultipleCharactersSafeCall(idList: String) {
+        multipleCharacters.value = NetworkResult.Loading()
+        if (hasInternetConnection()) {
+            try {
+                val response = repository.remote.getMultipleCharacters(idList)
+                multipleCharacters.value = handleMultipleCharactersResponse(response)
+            } catch (e: Exception) {
+                multipleCharacters.value = NetworkResult.Error("Characters Not Found")
+            }
+        } else {
+            multipleCharacters.value = NetworkResult.Error("No Internet Connection")
+        }
+    }
+
     private fun handleRickAndMortyResponse(response: Response<RickAndMortyResponse>): NetworkResult<RickAndMortyResponse> {
         when {
             response.code().toString() == "404" -> {
@@ -125,6 +146,21 @@ class MainViewModel @Inject constructor(
             response.isSuccessful -> {
                 val location = response.body()
                 return NetworkResult.Success(location!!)
+            }
+            else -> {
+                return NetworkResult.Error(response.message())
+            }
+        }
+    }
+
+    private fun handleMultipleCharactersResponse(response: Response<List<RickAndMortyCharacter>>): NetworkResult<List<RickAndMortyCharacter>> {
+        when {
+            response.code().toString() == "404" -> {
+                return NetworkResult.Error("Characters Not Found")
+            }
+            response.isSuccessful -> {
+                val multipleCharacters = response.body()
+                return NetworkResult.Success(multipleCharacters!!)
             }
             else -> {
                 return NetworkResult.Error(response.message())
