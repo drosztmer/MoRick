d76b52dac8ec901df77beee741aeaa1234ae2a9d
Drosztmér István <drosztmer.istvan@gmail.com>
2021-03-17 13:25:01 +0100

    Add binding, menu, viewmodel, adapters to favorites fragment

--- app/src/main/java/com/codecool/morick/ui/fragments/favoritecharacters/FavoriteCharactersFragment.kt
+++ app/src/main/java/com/codecool/morick/ui/fragments/favoritecharacters/FavoriteCharactersFragment.kt
@@ -1,23 +1,26 @@
 package com.codecool.morick.ui.fragments.favoritecharacters
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.codecool.morick.R
 import com.codecool.morick.adapters.CharactersAdapter
 import com.codecool.morick.databinding.FragmentFavoriteCharactersBinding
-import com.codecool.morick.util.Constants
 import com.codecool.morick.util.Constants.Companion.FAVORITES
 import com.codecool.morick.viewmodels.MainViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoriteCharactersFragment : Fragment() {
 
     private var _binding: FragmentFavoriteCharactersBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var mainViewModel: MainViewModel
+    private val mainViewModel: MainViewModel by viewModels()
     private val mAdapter by lazy { CharactersAdapter(FAVORITES) }
 
     override fun onCreateView(
@@ -30,7 +33,50 @@ class FavoriteCharactersFragment : Fragment() {
         binding.mainViewModel = mainViewModel
         binding.adapter = mAdapter
 
+        setHasOptionsMenu(true)
+
+        setupRecyclerView()
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.favorite_characters_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.delete_all_favorite_characters_menu) {
+            confirmDeleteAll()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun confirmDeleteAll() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton(getString(R.string.yes)) { _, _ ->
+            // mainViewModel.deleteAllFavorites()
+            showSnackBar()
+        }
+        builder.setNegativeButton(getString(R.string.no)) { _, _ -> }
+        builder.setTitle(getString(R.string.alert_dialog_title))
+        builder.setMessage(getString(R.string.alert_dialog_message))
+        builder.create().show()
+    }
+
+    private fun showSnackBar() {
+        Snackbar.make(binding.root, getString(R.string.successfully_deleted_everything), Snackbar.LENGTH_SHORT).setAction("Okay") {}.show()
+    }
+
+    private fun setupRecyclerView() {
+        binding.favoriteCharactersRecyclerView.apply {
+            adapter = mAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
